5d124a2027f169a43985837f0b86b5a3157dfb80
tesh5581 <1141327648@student.mmu.edu.my>
2016-02-14 15:46:08 +0800

    images weren't linking with folders. added Images/ dir

--- facilities.php
+++ facilities.php
@@ -67,10 +67,10 @@ a:active{
 <div id="section">
   <h1 style="text-align:center;">Facilities of MMU</h1>
   <p> </p>
-  <p align="center" style="text-align: center;"><strong> <img   src="recreational.jpg" /></strong></p>
+  <p align="center" style="text-align: center;"><strong> <img   src="Images/recreational.jpg" /></strong></p>
 
   <p style="text-align: center;font-size:160%"><strong>Horseback</strong></p>
-  <p align="center" style="text-align: center;"><strong> <img   src="horseback.jpg" /></strong></p>
+  <p align="center" style="text-align: center;"><strong> <img   src="Images/horseback.jpg" /></strong></p>
   <p style="text-align: center;">For more information about the Horseback Riding Club in UNITEN, please contact:</p>
   <p style="text-align: center;">EQUESTRIAN ASSOCIATION OF MALAYSIA (EAM)</p>
   <p style="text-align: center;">Contact: Maj (Rtd.) Yap Mou Soon, Executive Secretary</p>
@@ -82,13 +82,13 @@ a:active{
   <a href="mailto:someone@example.com?Subject=Inquiry" target="_top" style="color:blue">Send Mail</a></p>
 
   <p style="text-align: center;font-size:160%"><strong>Gym</strong></p>
-  <p align="center" style="text-align: center;"><strong> <img   src="Gym.jpg" /></strong></p>
+  <p align="center" style="text-align: center;"><strong> <img   src="Images/Gym.jpg" /></strong></p>
   <p style="text-align: center;">The MMU Gymnasium is located in the ground floor of the Faculty of Law building.</p>
   <p style="text-align: center;">The fee is RM1.05 per entry for student and RM1.60 for staff (inclusive of 6% GST). </p>
 
 
   <p style="text-align: center;font-size:160%"><strong>Pool</strong></p>
-  <p align="center" style="text-align: center;"><strong> <img   src="Pool.jpg" /></strong></p>
+  <p align="center" style="text-align: center;"><strong> <img   src="Images/Pool.jpg" /></strong></p>
   <p style="text-align: center;">MMU has an Olympic-sized swimming pool which is a part of the Campus sports and recreational facilities.</p>
   <p style="text-align: center;">The pool is open for students, staff, teaching groups as well as the public, with different opening hours allocated for staff,</p>
   <p style="text-align: center;">students, women and family groups.</p>
